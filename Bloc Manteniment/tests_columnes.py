a7cc8e73508b3ece77b1741494a7fe9a29ffb15d
arellanooangel <a.arellano@sapalomera.cat>
2026-05-08 15:23:22 +0200

    V3

--- Bloc Manteniment/tests_columnes.py
+++ Bloc Manteniment/tests_columnes.py
@@ -3,11 +3,11 @@ from auth_ok import get_connection
 def llistar_columnes():
     conn = get_connection()
     cur = conn.cursor()
-    # Canvia 'pacient' per 'hospital.pacient' si cal
+
     cur.execute("""
         SELECT column_name 
         FROM information_schema.columns 
-        WHERE table_name = 'pacient'
+        WHERE table_name = 'hospital.pacient'
     """)
     columnes = cur.fetchall()
     print("Les teves columnes reals són:")
