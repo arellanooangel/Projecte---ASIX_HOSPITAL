ee74af52666143776ef7040c8572a16e16f9ff6b
ullamas13 <llamasgonzalez.unai@gmail.com>
2026-05-14 16:43:10 +0200

    v9

--- Bloc Manteniment/auth_ok.py
+++ Bloc Manteniment/auth_ok.py
@@ -5,7 +5,7 @@ def get_connection():
     """Estableix la connexió amb la base de dades PostgreSQL."""
     try:
         return psycopg2.connect(
-            host="192.168.1.10",
+            host="192.168.10.10",
             database="hospital",
             user="ua-admin",
             password="admin123"
